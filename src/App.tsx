7758ed37dbca7047f64bf75ef63ca65a4501e8ae
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-19 15:30:14 +0600

    был добавлен интерфейс и присваивание уникального айди

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import TaskForm from './components/TaskForm.tsx';
 import TaskList from './components/TaskList.tsx';
 import './App.css';
 
+interface Task {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 const App: React.FC = () => {
-    const [tasks, setTasks] = useState<string[]>([]);
+    const [tasks, setTasks] = useState<Task[]>([]);
 
     const handleAddTask = (newTask: string) => {
-        setTasks([...tasks, newTask]);
+        const newTaskItem: Task = {
+            id: Math.random(),
+            title: newTask,
+            completed: false,
+        };
+        setTasks([...tasks, newTaskItem]);
     };
 
     return (
