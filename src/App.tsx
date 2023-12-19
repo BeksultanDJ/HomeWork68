a1eede323ee20b543ed42aaa74a23f000db63427
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-19 18:56:54 +0600

    сделал отправку событий в зранилище редукс

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,56 @@
-    import React, { useState } from 'react';
-    import TaskForm from './components/TaskForm.tsx';
-    import TaskList from './components/TaskList.tsx';
-    import { useDispatch } from 'react-redux';
-    import { addTask } from './components/taskSlice';
-    import './App.css';
+import React, { useState, useEffect } from 'react';
+import TaskForm from './components/TaskForm.tsx';
+import TaskList from './components/TaskList.tsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTask, fetchTasks, toggleTask, deleteTask } from './components/taskSlice';
+import './App.css';
 
-    interface Task {
-        id: number;
-        title: string;
-        completed: boolean;
-    }
+const App: React.FC = () => {
+    const [localTasks, setLocalTasks] = useState<{ id: number; title: string; completed: boolean }[]>([
+        { id: 1, title: 'Task 1', completed: false },
+        { id: 2, title: 'Task 2', completed: true },
+    ]);
+    const dispatch = useDispatch();
+    const tasks = useSelector((state: any) => state.tasks);
 
-    const App: React.FC = () => {
-        const [tasks, setTasks] = useState<{ id: number; title: string; completed: boolean }[]>([
-            { id: 1, title: 'Task 1', completed: false },
-            { id: 2, title: 'Task 2', completed: true },
-        ]);
-        const dispatch = useDispatch();
-
-        const handleAddTask = (newTask: string) => {
-            const newTaskItem: { completed: boolean; id: number; title: string } = {
-                completed: false,
-                id: Math.random(),
-                title: newTask,
-            };
-            setTasks([...tasks, newTaskItem]);
-            dispatch(addTask(newTaskItem));
+    const handleAddTask = (newTask: string) => {
+        const newTaskItem: { completed: boolean; id: number; title: string } = {
+            completed: false,
+            id: Math.random(),
+            title: newTask,
         };
-
-        const toggleTaskCompletion = (taskId: number) => {
-            setTasks(prevTasks => {
-                return prevTasks.map(task => {
-                    if (task.id === taskId) {
-                        return { ...task, completed: !task.completed };
-                    }
-                    return task;
-                });
-            });
-        };
-
-        const handleDeleteTask = (taskId: number) => {
-            setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
-        };
-
-        return (
-            <div className="AppContainer">
-                <h1>TODO App</h1>
-                <TaskForm onAddTask={handleAddTask} />
-                <TaskList tasks={tasks} onToggleTask={toggleTaskCompletion} onDeleteTask={handleDeleteTask} />
-            </div>
-        );
+        setLocalTasks(prevTasks => [...prevTasks, newTaskItem]);
+        dispatch(addTask(newTaskItem));
     };
 
-    export default App;
+    const toggleTaskCompletion = (taskId: number) => {
+        setLocalTasks(prevTasks => {
+            return prevTasks.map(task => {
+                if (task.id === taskId) {
+                    return { ...task, completed: !task.completed };
+                }
+                return task;
+            });
+        });
+        dispatch(toggleTask(taskId));
+    };
+
+    const handleDeleteTask = (taskId: number) => {
+        setLocalTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
+        dispatch(deleteTask(taskId));
+    };
+
+    useEffect(() => {
+        dispatch(fetchTasks());
+    }, [dispatch]);
+
+    return (
+        <div className="AppContainer">
+            <h1>TODO App</h1>
+            <TaskForm onAddTask={handleAddTask} />
+            <TaskList tasks={localTasks} onToggleTask={toggleTaskCompletion} onDeleteTask={handleDeleteTask} />
+        </div>
+    );
+};
+
+export default App;
